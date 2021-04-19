0cded6f82dff930924d3e43c1015c776c0eb442f
Gyeongmin Go <gyeongm@amazon.com>
2021-04-19 16:04:29 -0700

    update

--- go.mod
+++ go.mod
@@ -12,11 +12,12 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/onrik/gorm-logrus v0.3.0 // indirect
 	github.com/sirupsen/logrus v1.8.1
 	github.com/toorop/gin-logrus v0.0.0-20210225092905-2c785434f26f
 	github.com/ugorji/go v1.2.5 // indirect
 	golang.org/x/crypto v0.0.0-20210415154028-4f45737414dc // indirect
-	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c // indirect
+	golang.org/x/sys v0.0.0-20210419170143-37df388d1f33 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/mysql v1.0.5 // indirect
--- repo/open_db.go
+++ repo/open_db.go
@@ -1,6 +1,7 @@
 package repo
 
 import (
+	gorm_logrus "github.com/onrik/gorm-logrus"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,13 +10,15 @@ import (
 
 type DDB struct {
 	db  *gorm.DB
-	log *logrus.Logger
 }
 
 const DSN = "shortener:shortener@tcp(127.0.0.1:3306)/shortener?charset=utf8mb4&parseTime=True&loc=Local"
 
 func New(log *logrus.Logger) *DDB {
-	if db, err := gorm.Open(mysql.Open(DSN), &gorm.Config{}); err != nil {
+	logrus.SetLevel(logrus.DebugLevel)
+	if db, err := gorm.Open(mysql.Open(DSN), &gorm.Config{
+		Logger: gorm_logrus.New(),
+	}); err != nil {
 		log.Fatalf("Failed to open server: %v", err)
 		panic("Can't reach here")
 	} else if err := db.AutoMigrate(&model.Url{}); err != nil {
@@ -24,15 +27,10 @@ func New(log *logrus.Logger) *DDB {
 	} else {
 		return &DDB{
 			db:  db,
-			log: log,
 		}
 	}
 }
 
 func (r *DDB) DB() *gorm.DB {
 	return r.db
-}
-
-func (r *DDB) Log() *logrus.Logger {
-	return r.log
 }
